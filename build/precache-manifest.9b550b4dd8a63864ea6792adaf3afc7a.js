73d9bea4c64680d8f3fdd8b40c75e818bdace77b
Andi Cirjaliu <Andi.Cirjaliu@ro.ibm.com>
2020-09-28 17:54:06 +0300

    created build

---
+++ build/precache-manifest.9b550b4dd8a63864ea6792adaf3afc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2923fcd381314a169d6206b1e9f4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b5b4b863df59b889941",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "6e1235610f901dab679f",
+    "url": "/static/js/2.ab9636f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab9636f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5b4b863df59b889941",
+    "url": "/static/js/main.20759d30.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const buildPath = path.join(__dirname, "build");
 console.log('React app build path: ', buildPath);
 server.use(express.static( buildPath));
 
+//Handle the health check
+server.use('/health', (req, res, next) => {
+    console.log('Health test...');
+    res.status(200).json({"msg": "The application is running"});
+  });
+
 server.use("/*", (req, res) =>{
+    console.log('requested ', req.url);
     res.sendFile(path.join(buildPath, 'index.html'));
 });
 
